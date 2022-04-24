3581835e33a023cf0f400fe5c7f8b406bf74715f
ysaad01 <ysaad2325@gmail.com>
2022-04-23 23:22:41 -0400

    linked front end styles from public folder

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const { animals } = require('./data/animals.json')
 const express = require('express');
+const res = require('express/lib/response');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // parse incoming string or array data 
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -112,6 +114,19 @@ app.post('/api/animals', (req, res) => {
   
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
